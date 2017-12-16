0e0700aa172f31e7493a567d2fb482f6df1302b1
Ricardo Kury <ricardokury@outlook.com>
2017-12-16 13:01:43 -0200

    Add project files.

---
+++ MySocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLacuna
+{
+    class MySocket
+    {
+        private const String token = "1d0b1da2-0971-4236-9697-4f2a09588d6c";
+        private const String address = "lacuna.ddns.net";
+        private const Int32 port = 21820;
+        private TcpClient client;
+        public MySocket() => client = new TcpClient(address, port);
+
+        public void SendMessage(Byte[] message)
+        {
+            var stream = client.GetStream();
+            stream.Write(message, 0, message.Length);
+            Console.WriteLine(String.Format("Sent >> {0}", BitConverter.ToString(message)));
+        }
+
+        public int ReadMessage(Byte[] read_data)
+        {
+            var stream = client.GetStream();
+            return stream.Read(read_data, 0, read_data.Length);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoLacuna;
+
+namespace ProjetoLacuna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new MySocket();
+            var response = new Byte[256];
+            while (true)
+            {
+                Console.WriteLine("What message do you want to send?");
+                var message = Console.ReadLine();
+                socket.SendMessage(Encoding.ASCII.GetBytes(message));
+                var byte_count = socket.ReadMessage(response);
+                var str_response = Encoding.ASCII.GetString(response, 0, byte_count);
+                Console.WriteLine(String.Format("Received (bytes) << {0}", BitConverter.ToString(response)));
+                Console.WriteLine(String.Format("Received (string) << {0}", str_response));
+            }
+            //Byte[] bytes = { 1, 2, 3 };
+            //Console.WriteLine(BitConverter.ToString(bytes));
+            //Console.Read();
+        }
+    }
+}
