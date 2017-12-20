47ac417ef11e73762a0e4a9881f740138980547d
Ricardo Kury <ricardokury@outlook.com>
2017-12-20 20:53:36 -0200

    Verifies the integrity of the message (computing checksum)

--- MySocket.cs
+++ MySocket.cs
@@ -8,46 +8,59 @@ using System.Threading.Tasks;
 
 namespace ProjetoLacuna
 {
-    class MySocket
+    class EmpireSocket
     {
         private const String token = "1d0b1da2-0971-4236-9697-4f2a09588d6c";
         private const String address = "lacuna.ddns.net";
         private const Int32 port = 21820;
         private TcpClient client;
-        private StreamReader streamReader;
-        private StreamWriter streamWriter;
 
-        public MySocket()
+        public EmpireSocket()
         {
             client = new TcpClient(address, port);
-            streamReader = new StreamReader(client.GetStream());
-            streamWriter = new StreamWriter(client.GetStream());
         }
 
-        public void SendMessage(Byte[] message)
+        public void SendMessage(String message)
         {
             var stream = client.GetStream();
-            stream.Write(message, 0, message.Length);
-            Console.WriteLine(String.Format("Sent >> {0}", BitConverter.ToString(message)));
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+            stream.Write(bytes, 0, bytes.Length);
+            Console.WriteLine(String.Format("Sent >> {0}", message));
         }
 
         public int ReadMessage(Byte[] read_data)
         {
-            int bytes_read = 0;
             var stream = client.GetStream();
-            // Read at least 2 bytes representing the message size
-            while (bytes_read < 2)
+            while (true)
             {
-                bytes_read += stream.Read(read_data, bytes_read, read_data.Length - bytes_read);
+                int bytes_read = 0;
+                // Read at least 2 bytes representing the message size
+                while (bytes_read < 2)
+                {
+                    bytes_read += stream.Read(read_data, bytes_read, read_data.Length - bytes_read);
+                }
+                // This doesn't work reliably because of endianness
+                //var payload_size = BitConverter.ToInt16(read_data, 0);
+                int payload_size = (read_data[0] << 8) | (read_data[1]);
+                while (bytes_read < payload_size + 2) // Payload size does not include the first 2 bytes, hence +2
+                {
+                    bytes_read += stream.Read(read_data, bytes_read, read_data.Length - bytes_read);
+                }
+                if (VerifyChecksum(read_data, bytes_read))
+                {
+                    return bytes_read;  
+                }
+                else
+                {
+                    SendMessage("send again"); 
+                }
             }
-            // This doesn't work reliably because of endianness
-            //var payload_size = BitConverter.ToInt16(read_data, 0);
-            int payload_size = (read_data[0] << 8) | (read_data[1]);
-            while (bytes_read < payload_size)
-            {
-                bytes_read += stream.Read(read_data, bytes_read, read_data.Length - bytes_read);
-            }
-            return bytes_read;
+        }
+
+        private bool VerifyChecksum(Byte[] data, int size)
+        {
+            byte checksum = data.Take(size - 1).Aggregate((x, y) => (byte)(x + y));
+            return (checksum == data[size - 1]);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,20 +11,22 @@ namespace ProjetoLacuna
     {
         static void Main(string[] args)
         {
-            var socket = new MySocket();
+            var socket = new EmpireSocket();
             var response = new Byte[256];
             while (true)
             {
                 Console.WriteLine("What message do you want to send?");
                 var message = Console.ReadLine();
-                socket.SendMessage(Encoding.ASCII.GetBytes(message));
+                socket.SendMessage(message);
                 var byte_count = socket.ReadMessage(response);
                 var str_response = Encoding.ASCII.GetString(response, 0, byte_count);
                 Console.WriteLine(String.Format("Received (bytes) << {0}", BitConverter.ToString(response)));
                 Console.WriteLine(String.Format("Received (string) << {0}", str_response));
             }
-            //Byte[] bytes = { 1, 2, 3 };
-            //Console.WriteLine(BitConverter.ToString(bytes));
+            //byte[] data = { 1, 2, 200, 50, 200, 200, 6 };
+            //var checksum = data.Skip(2).Take(data.Length - 3).Aggregate((x, y) => (byte)(x + y));
+            //Console.WriteLine(650 & 0xFF);
+            //Console.WriteLine(checksum);
             //Console.Read();
         }
     }
