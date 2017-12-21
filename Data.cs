ba58849231b309065e77512cf7aeac638b80fad7
Ricardo Kury <ricardokury@outlook.com>
2017-12-20 23:28:21 -0200

    Create class to store and convert data as bytes or ASCII strings.

---
+++ Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace ProjetoLacuna
+{
+    class Data
+    {
+        public byte[] Bytes;
+        public String Str
+        {
+            get { return Encoding.ASCII.GetString(Bytes); }
+            set { Bytes = Encoding.ASCII.GetBytes(value); }
+        }
+
+        public Data(byte[] data) => Bytes = data;
+        public Data(String data) => Str = data;
+    }
+}
